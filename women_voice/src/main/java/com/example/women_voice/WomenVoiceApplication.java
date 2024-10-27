a3cc468f740d01cf85962223a290e5b000f9ad6b
saikalbatyrbekova <132566790+saikalbatyrbekova@users.noreply.github.com>
2024-10-27 18:33:58 +0600

    Initial commit

---
+++ women_voice/src/main/java/com/example/women_voice/WomenVoiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.women_voice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WomenVoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WomenVoiceApplication.class, args);
+	}
+
+}
---
+++ women_voice/src/test/java/com/example/women_voice/WomenVoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.women_voice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WomenVoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
